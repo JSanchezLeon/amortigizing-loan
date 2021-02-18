f68662732358099f7de141f77e53cfdce05edbf5
Jorge <jorgels.leon@gmail.com>
2021-02-18 00:14:24 +0000

    Added more functionality to ABS class

--- AmortizingLoan.py
+++ AmortizingLoan.py
@@ -162,11 +162,13 @@ class ABS():
         self.min = min_balance
         self.max = max_balance
         self.balances = [random.randint(self.min,self.max) for loan in range(self.n_loans)]
-        self.asset_prices = [balances - random.randint(10e3,20e3) for balances in self.balances]
-        self.maturities = [random.choice([15,30]) for maturity in range(self.n_loans)]
+        self.asset_prices = [balances + random.randint(10e3,20e3) for balances in self.balances]
+        self.maturities = [random.choice([15*12,30*12]) for maturity in range(self.n_loans)]
         self.interests = [random.randint(3,11)/100 for rate in range(self.n_loans)]
 
-    
+    def __str__(self):
+        return "number of loans is {}".format(self.n_loans)
+
     def generate_pool(self):
         loans = list(zip(self.asset_prices,self.balances,self.interests,self.maturities))
         pool = [AmortizingLoan(loan[0], loan[1], loan[2], loan[3]) \
@@ -178,18 +180,24 @@ class ABS():
         for i in self.pool:
             print(i.get_payment())
         return 
+
     def calculate_abs_cashflows(self):
-        self.cashflow = float()
+        self.cashflow, self.ltv, self.total_balance, self.wam, self.wac = 0, 0, 0, 0, 0
         for loan in self.pool:
             self.cashflow += loan.get_payment()
-        return self.cashflow
+            self.ltv += loan.get_LTV() * (1/self.n_loans)
+            self.total_balance += loan.principal
+        for loan in self.pool:
+            self.wam += loan.maturity * (loan.principal/self.total_balance)
+            self.wac += loan.interest * (loan.principal/self.total_balance)
+    
+        return self.cashflow, self.ltv, self.total_balance, self.wam, self.wac
 
 
 
-abs1 = ABS(10, 10e4, 20e4)
-print(abs1.generate_pool())
-abs1.print_payments()
-print(abs1.calculate_abs_cashflows())
+abs1 = ABS(int(10e3), 10e4, 20e4)
+abs1.generate_pool()
+#print(abs1.calculate_abs_cashflows())
 
 
         
